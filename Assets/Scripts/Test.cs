b0ef82aa8c6005b24458d6d4e4a4a3d4565ff151
joshuapink2002 <83262978+ArtTechSooKim@users.noreply.github.com>
2025-11-04 19:00:25 +0900

    Move: Imported assets into ExternalAssets folder (organized)

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.Audio;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
